883edf749477e456df10868879d29e92deed9767
Oleg Lebediev <lebedyevoleg@gmail.com>
2014-02-04 13:25:47 -0500

    Set-up web-pages for educators
    Assigned roles in the database, and the layout

---
+++ Project/Project/Controllers/GeneralController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Controllers
+{
+    public class GeneralController : Controller
+    {
+        //
+        // GET: /General/
+
+        public ActionResult ADHD()
+        {
+            return View();
+        }
+
+        public ActionResult HD()
+        {
+            return View();
+        }
+
+        public ActionResult Dd()
+        {
+            return View();
+        }
+
+        public ActionResult TC()
+        {
+            return View();
+        }
+    }
+}
